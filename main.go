af7a453ea70db19e2e518987d03b5d1a7f5d9ea6
Felipe Silva <fsilva1985@gmail.com>
2021-09-08 09:19:46 -0300

    fixed failure

--- main.go
+++ main.go
@@ -29,15 +29,23 @@ func main() {
 }
 
 func generate(source string, output string) {
-	taskCtx, cancel := chromedp.NewContext(
-		context.Background(),
-		chromedp.WithLogf(log.Printf),
-	)
-	defer cancel()
 	var pdfBuffer []byte
+
+	opts := []chromedp.ExecAllocatorOption{
+		chromedp.NoSandbox,
+		chromedp.Headless,
+	}
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	taskCtx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
 	if err := chromedp.Run(taskCtx, getContent(source, "html", &pdfBuffer)); err != nil {
 		log.Fatal(err)
 	}
+
 	if err := ioutil.WriteFile(output, pdfBuffer, 0644); err != nil {
 		log.Fatal(err)
 	}
@@ -53,7 +61,9 @@ func getContent(source string, sel string, res *[]byte) chromedp.Tasks {
 			if err != nil {
 				return err
 			}
+
 			*res = buf
+
 			return nil
 		}),
 	}
